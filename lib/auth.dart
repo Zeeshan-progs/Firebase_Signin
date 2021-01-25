d521eb3d6eb8abcb4fc180b4fa73f26a8ba6719a
Zeeshan-progs <md.zeeshaniqbal7277@gmail.com>
2021-01-25 17:49:57 +0530

    email

--- lib/auth.dart
+++ lib/auth.dart
@@ -5,6 +5,10 @@ class UserSignin {
 
   // method is type of future
   // so
+  UserUid _uidFromFirebase(User user) {
+    return user != null ? UserUid(uid: user.uid) : null;
+  }
+
   Future signInAnon() async {
     try {
       // using async because future do take some time to complete given work
@@ -12,11 +16,13 @@ class UserSignin {
           await FirebaseAuth.instance.signInAnonymously();
       // in new update of firebase you need to add usercredential at place of AuthResult
       // now you can directly return userCredential
-      print(userCredential.user);
-      return userCredential;
+      User user = userCredential.user;
+      print(user.uid);
+      return _uidFromFirebase(user);
       // in any exception error lets do all its work in try catch block}
     } catch (e) {
-      print(e);
+      print(e.toString());
+      return null;
     }
   }
 
@@ -27,6 +33,40 @@ class UserSignin {
       await FirebaseAuth.instance.signOut();
     } catch (e) {
       print(e);
+    }
+  }
+}
+
+class UserUid {
+  final String uid;
+  UserUid({this.uid});
+}
+
+class EmailSign {
+  String email, password;
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  Future signinWithEmail(String email, String password) async {
+    try {
+      if (email != null && password != null) {
+        dynamic user =
+            auth.signInWithEmailAndPassword(email: email, password: password);
+        return user;
+      } else {
+        print('User not created ');
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future createEmail(String email, String password) async {
+    try {
+      dynamic user =
+          auth.createUserWithEmailAndPassword(email: email, password: password);
+      return user;
+    } catch (e) {
+      print(e.toString());
     }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:sign_anon/EmailSign.dart';
 import 'package:sign_anon/auth.dart';
 
 Future<void> main() async {
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Sign(),
+      home: SignEmail(),
     );
   }
 }
